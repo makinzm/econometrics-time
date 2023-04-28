36242b67c7281d21a6ad5942f53846a5e5744c60
makinzm <yuruseba@yahoo.co.jp>
2023-04-28 17:42:10 +0900

    set tex-environment and some libraries

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -it -p 8787:8787 -v $PWD/src:/home/rstudio/src -e PASSWORD=p my_rstudio
+docker run -it -p 8787:8787 -v $PWD/src:/home/rstudio/src -e PASSWORD=p r_env
 
 ## username=rstudio, password=p
 # rocker/rstudio:4.1
